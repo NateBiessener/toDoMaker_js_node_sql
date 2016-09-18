095dec8e6336ad609a89bf10e241e6aa19d98ad0
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-17 23:37:46 -0500

    add sinew to the skeleton

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,3 +3,32 @@ console.log('script sourced');
 $(document).ready(function(){
   console.log('jq');
 })
+
+var objectToSend = {};
+
+$.ajax({
+  url: '/addLine',
+  type: 'POST',
+  data: objectToSend,
+  success: function(data){
+    console.log('in addLine ajax success');
+  }
+});
+
+$.ajax({
+  url: '/editLine',
+  type: 'POST',
+  data: objectToSend,
+  success: function(data){
+    console.log('in editLine ajax success');
+  }
+});
+
+$.ajax({
+  url: '/deleteLine',
+  type: 'POST',
+  data: objectToSend,
+  success: function(data){
+    console.log('in deleteLine ajax success');
+  }
+});
--- server/app.js
+++ server/app.js
@@ -16,4 +16,69 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('public/index.html'));
 })
 
+app.post('/addLine', urlencodedParser, function(req, res){
+  console.log('addLine url hit');
+  //add line to database
+  var queryString = 'INSERT INTO task (description, complete, priority) VALUES ($1, $2, $3)', [/*$1 $2 $3 TO BE ADDED*/];
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log(err);
+    }
+    else {
+      client.query(queryString);
+      var responseArray = [];
+      var responseQuery = client.query('SELECT FROM task WHERE id = $1', [/*$1 TO BE ADDED*/]);
+      responseQuery.on('row', function(row){
+        responseArray.push(row);
+      });//end on row
+      responseQuery.on('end', function(){
+        done();
+        //return added line
+        return res.json(responseArray);
+      });//end end
+    }//end else
+  });//end pg.connect
+});//end /addLine
+
+app.post('/editLine', urlencodedParser, function(req, res){
+  console.log('editLine url hit');
+  //change provided propertie/s of line in database
+  var queryString = 'UPDATE task SET ' + /*THINGS = THINGS*/ 'WHERE id = $1', [/*$1 TO BE ADDED*/];
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log(err);
+    }
+    else {
+      client.query(queryString);
+      var responseArray = [];
+      var responseQuery = client.query('SELECT FROM task WHERE id = $1', [/*$1 TO BE ADDED*/]);
+      responseQuery.on('row', function(row){
+        responseArray.push(row);
+      });//end on row
+      responseQuery.on('end', function(){
+        done();
+        //return changed line
+        return res.json(responseArray);
+      });//end end
+    }//end else
+  });//end pg.connect
+});//end /editLine
+
+app.post('/deleteLine', urlencodedParser, function(req, res){
+  console.log('deleteLine url hit');
+  //remove line from database
+  var queryString = 'DELETE FROM task WHERE id = $1', [/*$1 TO BE ADDED*/];
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log(err);
+    }
+    else {
+      client.query(queryString);
+      done();
+      //return success
+      res.send();
+    }//end else
+  });//end pg.connect
+});// end /deleteLine
+
 app.use(express.static('public'));
